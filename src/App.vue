e2af565a00f62bd0da7fa2c423f4ec83f3f83a4e
deibo <deib0@foxmail.com>
2022-07-23 17:09:35 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Deibo/>
+</template>
+
+<script>
+import Deibo from './components/Deibo.vue'
+
+export default {
+  name: 'App',
+  components: {
+   Deibo 
+  }
+}
+</script>
---
+++ src/components/Deibo.vue
@@ -0,0 +1,9 @@
+<template>
+    <span>这里是deibo的组件，欢迎光临</span>
+</template>
+
+<script>
+export default {
+    name:'Deibo'
+}
+</script>
