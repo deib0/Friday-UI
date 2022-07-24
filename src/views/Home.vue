a6720996c089afdb34ea00f83497f48fddeea502
deibo <deib0@foxmail.com>
2022-07-24 16:15:22 +0800

    更新Home页面的样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,27 +15,30 @@
 <script lang="ts">
 import Topnav from "../components/Topnav.vue"
 export default {
-    components:{ Topnav}
+    components:{ Topnav }
 }
 </script>
 
 <style lang="scss" scoped>
-    .topnav {
-        border: 1px solid #000;
-        background: pink;
+    .banner {
+        padding: 100px 0;
         display: flex;
-        padding: 16px;
-        > .logo {
-            max-width: 6em;
-            margin-right: auto;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        background: lightgreen;
+        > .actions{
+            padding: 8px 0;
+            a {
+                margin:0 8px;
+                background: #fff;
+                display: inline-block;
+                $h: 28px;
+                height: $h;
+                line-height: $h;
+                border-radius: $h/2;
+                padding: 0 8px;
+            }
         }
-        > .menu {
-            display: flex;
-            white-space: nowrap;
-            flex-wrap: nowrap;
-        > li {
-            margin: 0 1em;
-        }
-    } 
-}
+    }
 </style>
