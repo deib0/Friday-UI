68405e828613580ab5172646343b5b98ddefb645
deibo <deib0@foxmail.com>
2022-07-24 16:03:11 +0800

    topnav样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
         <div class="topnav">
-            <div class="logo"></div>
-            <div class="menu"></div>
+            <div class="logo">LOGO</div>
+            <ul class="menu">
+                <li>menu1</li>
+                <li>menu2</li>
+            </ul>
         </div>
         <div class="banner">
             <h1>轮子UI</h1>
@@ -22,3 +25,24 @@ export default {
     },
 }
 </script>
+
+<style lang="scss" scoped>
+    .topnav {
+        border: 1px solid #000;
+        background: pink;
+        display: flex;
+        padding: 16px;
+        > .logo {
+            max-width: 6em;
+            margin-right: auto;
+        }
+        > .menu {
+            display: flex;
+            white-space: nowrap;
+            flex-wrap: nowrap;
+        > li {
+            margin: 0 1em;
+        }
+    } 
+}
+</style>
