e7aa09cfd3ed48357a437ebc320605eeef45ef20
deibo <deib0@foxmail.com>
2022-07-24 22:04:05 +0800

    基本完成官网框架

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import App from "./App.vue";
 import "./index.scss";
 import { router } from './router';
 
-const app =createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <Topnav />
     <div class="banner">
       <h1>轱辘UI</h1>
-      <h2>一个好用且优雅的 UI 框架</h2>
+      <h2>一个厉害的 UI 框架</h2>
       <p class="actions">
         <a href="https://github.com">GitHub</a>
         <router-link to="/doc">开始</router-link>
