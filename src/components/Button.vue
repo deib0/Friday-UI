4443a578ac1d3829d3942d77b92246295653c01a
deibo <deib0@foxmail.com>
2022-07-26 10:05:42 +0800

    完成Button基础样式

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,9 +1,7 @@
 <template>
-    <div>        
-        <button :class="`them-${theme}`">
-            <slot/>
-        </button>
-    </div>
+            <button class="friday-button" :class="`them-${theme}`">
+                <slot/>
+            </button>
 </template>
 
 <script lang="ts">
@@ -18,3 +16,40 @@ export default {
     }
 }
 </script>
+<style lang="scss" scoped>
+
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.friday-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {// 兼容
+    border: 0;
+  }
+}
+</style>
