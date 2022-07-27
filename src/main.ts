34e005c24d5dea0a092bea9798b641cf0fd996db
deibo <deib0@foxmail.com>
2022-07-27 19:37:33 +0800

    完成指示横条的样式

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
+import "./lib/friday.scss"
+import "./index.scss";
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./index.scss";
-import "./lib/friday.scss"
 import { router } from './router';
-
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
