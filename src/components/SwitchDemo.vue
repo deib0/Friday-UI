175fc094bd8026d764aff40d896c6cb0c56a4ec9
deibo <deib0@foxmail.com>
2022-07-29 18:05:54 +0800

    完成SwtichDemo示例的样式

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    <Switch v-model:checked="checked" />
-  </div>
+  <h1>Switch 组件示例</h1>
+  <div class="demo">
+    <h2>常规用法</h2>
+    <div class="demo-component">
+      <Switch v-model:checked="checked" />
+    </div>
+    <div class="demo-actions">
+      <Button>查看代码</Button>
+    </div>
+    <div class="demo-code">
+      <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+    </div>
 </template>
 
 <script lang="ts">
@@ -15,3 +24,38 @@ export default {
     components: { Switch }
 }
 </script>
+
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  >h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    >button{
+      background: #fff;
+      border: 1px solid #000;
+      padding: 3px;
+      cursor: pointer;
+    }
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    >pre {
+      line-height: 1.1;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+      margin: 0;
+    }
+  }
+}
+</style>
